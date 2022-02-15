dc09b35161a2232c740fd5e2ac0af902e872ee5e
RichardCuthbert <RichardCuthbertnc@protonmail.com>
2022-02-15 12:28:08 +0000

    respond with messages object from the server

--- index.js
+++ index.js
@@ -5,25 +5,30 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get("/", (req, res) => {
-	res.send("Welcome to the chat room");
-});
+app.use(express.json());
 
-const messages = [{ username: "", message: "" }];
+const messages = [
+  { username: "ynyn", avatar: "ghngn", message: "gngh" },
+  { username: "ynyn", avatar: "ghngn", message: "gngh" },
+  { username: "ynyn", avatar: "ghngn", message: "gngh" },
+];
 
 io.on("connection", (socket) => {
-	console.log("You are connected");
-	socket.on("chat message", (msg) => {
-		messages.push(msg);
-		io.emit("chat message", messages);
-	});
-	socket.on("disconnect", () => {
-		console.log("user disconnected");
-	});
+  console.log("You are connected");
+  socket.send(messages);
+  socket.on("chat message", (msg) => {
+    messages.push(msg);
+    io.emit("chat message", messages);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 const { PORT = 3000 } = process.env;
 
 server.listen(PORT, () => {
-	console.log("listening...");
+  console.log("listening...");
 });
+
+module.exports = app;
